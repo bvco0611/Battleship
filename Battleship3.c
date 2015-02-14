4c9f73b4831327673516ee1340f0192aff192631
brandon66 <brandon.mcbride@trojans.dsu.edu>
2015-02-13 21:41:18 -0600

    Update Battleship3.c
    Created a function printed ocean before having user place their ships

--- Battleship3.c
+++ Battleship3.c
@@ -3,6 +3,8 @@ Brandon McBride, Curt Rueckert, Jesse McGraw
 2-3-2015
 Battleship Program
 */
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -22,254 +24,172 @@ const int HIT      = 3;  // shot and hit
 
 //function prototypes
 void printIntro(void);
-void placeShip(int ocean[ROWS][COLS],char player[]);
 void resetOcean(int ocean[ROWS][COLS]);
+void placeShip(int ocean[ROWS][COLS],char player[]);
 void printOcean(int ocean[ROWS][COLS],char player[]);
 
 
 int main(void)
 {
+    //setup random seed time
+    srand(time(NULL));
+    //variables
+    int wone = 0, wtwo = 0;
+    int f1, f2;
+    int randomNumber;
+    char Junk[16]; // buffer for discarding excessive user input,
+    // used by "FLUSH_STDIN" macro
 
-	
-	int wone = 0, wtwo = 0;
-	int f1, f2;
-	//setup random seed time
-	srand(time(NULL));
+    //should be long enough for a first name
+    char playerOne[NAME_SIZE];
+    char playerTwo[NAME_SIZE];
 
-	//variables
-	int randomNumber;
-	char Junk[16]; // buffer for discarding excessive user input,
-                   // used by "FLUSH_STDIN" macro
+    // array for the "Ocean"
+    int oceanOne[ROWS][COLS];
+    int oceanTwo[ROWS][COLS];
 
-	//should be long enough for a first name
-	char playerOne[NAME_SIZE];
-	char playerTwo[NAME_SIZE];
-
-	// array for the "Ocean"
-	int oceanOne[ROWS][COLS];
-	int oceanTwo[ROWS][COLS];
-
-	//initialize the array with 0's(reset ocean)
-	resetOcean(oceanOne);
-	resetOcean(oceanTwo);
+    //initialize the array with 0's(reset ocean)
+    resetOcean(oceanOne);
+    resetOcean(oceanTwo);
 
 
-	//get user input
+    //get user input
 
-	//Ask the players to enter their name.
-	printf("Player 1 please enter your name: ");
-	fgets(playerOne,sizeof(playerOne),stdin);
-	FLUSH_STDIN(playerOne); // strip newline, flush extra chars
+    //Ask the players to enter their name.
+    printf("Player 1 please enter your name: ");
+    fgets(playerOne,sizeof(playerOne),stdin);
+    FLUSH_STDIN(playerOne); // strip newline, flush extra chars
 
-	printf("Player 2 please enter your name: ");
-	fgets(playerTwo,sizeof(playerTwo),stdin);
+    printf("Player 2 please enter your name: ");
+    fgets(playerTwo,sizeof(playerTwo),stdin);
     FLUSH_STDIN(playerTwo); // strip newline, flush extra chars
 
 
+
+
     //have players place their ships
- 	placeShip(oceanOne,playerOne);
- 	placeShip(pceanTwo,PlayerTwo);
+    printOcean(oceanOne,playerOne);
+    placeShip(oceanOne,playerOne);
+    printOcean(oceanTwo,playerTwo);
+    placeShip(oceanTwo,playerTwo);
 
 
 
-	//randomly select a player to start.
-	randomNumber = (rand() % 10)+1;
-	if (randomNumber > 5)
-	{
-		printf("%s will start\n", playerOne);
-		while(wone <= 2 || wtwo <= 2)
-		{
-			printf("%s FIRE!!: ", playerOne);
-			scanf("%d %d", &f1, &f2);
-			while(oceanTwo[f1][f2] == MISSED || oceanTwo[f1][f2] == HIT)
-			{
-				printf("Position already fired upon!: ");
-				scanf("%d %d", &f1, &f2);
-			}
-			if(oceanTwo[f1][f2] == OCCUPIED)
-			{
-				printf("That's a hit!\n");
-				oceanTwo[f1][f2] = HIT;
-				wone++;
-			}
-			else
-			{
-				printf("Miss.\n");
-				oceanTwo[f1][f2] = MISSED;
-			}
-			printOcean(oceanTwo,playerTwo);
-			printf("\n\n");
-			printf("%s FIRE!!: ", playerTwo);
-			scanf("%d %d", &f1, &f2);
-			while(oceanOne[f1][f2] == MISSED || oceanOne[f1][f2] == HIT)
-			{
-				printf("Position already fired upon!: ");
-				scanf("%d %d", &f1, &f2);
-			}
-			if(oceanOne[f1][f2] == OCCUPIED)
-			{
-				printf("That's a hit!\n");
-				oceanOne[f1][f2] = HIT;
-				wtwo++;
-			}
-			else
-			{
-				printf("Miss.\n");
-				oceanOne[f1][f2] = MISSED;
-			}
-			printOcean(oceanOne,playerOne);
-		}	
-	}
-	else if (randomNumber < 5)
-	{
-		printf("%s will start\n", playerTwo);
-		while(wone <= 2 || wtwo <= 2)
-		{
-			printf("%s FIRE!!: ", playerTwo);
-			scanf("%d %d", &f1, &f2);
-			while(oceanOne[f1][f2] == MISSED || oceanOne[f1][f2] == HIT)
-			{
-				printf("Position already fired upon!: ");
-				scanf("%d %d", &f1, &f2);
-			}
-			if(oceanOne[f1][f2] == OCCUPIED)
-			{
-				printf("That's a hit!\n");
-				oceanOne[f1][f2] = HIT;
-				wtwo++;
-			}
-			else
-			{
-				printf("Miss.\n");
-				oceanOne[f1][f2] = MISSED;
-			}
-			printOcean(oceanTwo,playerTwo);
-			printf("\n\n");
-			printf("%s FIRE!!: ", playerOne);
-			scanf("%d %d", &f1, &f2);
-			while(oceanTwo[f1][f2] == MISSED || oceanTwo[f1][f2] == HIT)
-			{
-				printf("Position already fired upon!: ");
-				scanf("%d %d", &f1, &f2);
-			}
-			if(oceanTwo[f1][f2] == OCCUPIED)
-			{
-				printf("That's a hit!\n");
-				oceanTwo[f1][f2] =	HIT;
-				wone++;
-			}
-			else
-			{
-				printf("Miss.\n");
-				oceanTwo[f1][f2] = MISSED;
-			}
-			printOcean(oceanOne,playerOne);
-			printf("\n\n");
-		}	
-	}
-	
-	if(wone == 3)
-	{
-		printf("%s Won the Game!!\n", playerOne);
-	}
-	else if(wtwo == 3)
-	{
-		printf("%s Won the Game!!\n", playerTwo);
-	}
-	
+    //randomly select a player to start.
+    randomNumber = (rand() % 10)+1;
+    if (randomNumber > 5)
+    {
+        printf("%s will start\n", playerOne);
+        while(wone < 2 || wtwo < 2)
+        {
+            printf("%s FIRE!!: ", playerOne);
+            scanf("%d %d", &f1, &f2);
+
+
+            while(oceanTwo[f1][f2] == MISSED || oceanTwo[f1][f2] == HIT)
+            {
+                printf("Position already fired upon!: ");
+                scanf("%d %d", &f1, &f2);
+            }
+            if(oceanTwo[f1][f2] == OCCUPIED)
+            {
+                printf("That's a hit!\n");
+                oceanTwo[f1][f2] = HIT;
+                wone++;
+            }
+            else
+            {
+                printf("Miss.\n");
+                oceanTwo[f1][f2] = MISSED;
+            }
+
+            printf("%s FIRE!!: ", playerTwo);
+            scanf("%d %d", &f1, &f2);
+            while(oceanOne[f1][f2] == MISSED || oceanOne[f1][f2] == HIT)
+            {
+                printf("Position already fired upon!: ");
+                scanf("%d %d", &f1, &f2);
+            }
+            if(oceanOne[f1][f2] == OCCUPIED)
+            {
+                printf("That's a hit!\n");
+                oceanOne[f1][f2] = HIT;
+                wtwo++;
+            }
+            else
+            {
+                printf("Miss.\n");
+                oceanOne[f1][f2] = MISSED;
+            }
+        }
+    }
+    else if (randomNumber < 5)
+    {
+        printf("%s will start\n", playerTwo);
+        while(wone < 2 || wtwo < 2)
+        {
+            printf("%s FIRE!!: ", playerTwo);
+            scanf("%d %d", &f1, &f2);
+            while(oceanOne[f1][f2] == MISSED || oceanOne[f1][f2] == HIT)
+            {
+                printf("Position already fired upon!: ");
+                scanf("%d %d", &f1, &f2);
+            }
+            if(oceanOne[f1][f2] == OCCUPIED)
+            {
+                printf("That's a hit!\n");
+                oceanOne[f1][f2] = HIT;
+                wtwo++;
+            }
+            else
+            {
+                printf("Miss.\n");
+                oceanOne[f1][f2] = MISSED;
+            }
+
+            printf("%s FIRE!!: ", playerOne);
+            scanf("%d %d", &f1, &f2);
+            while(oceanTwo[f1][f2] == MISSED || oceanTwo[f1][f2] == HIT)
+            {
+                printf("Position already fired upon!: ");
+                scanf("%d %d", &f1, &f2);
+            }
+            if(oceanTwo[f1][f2] == OCCUPIED)
+            {
+                printf("That's a hit!\n");
+                oceanTwo[f1][f2] = HIT;
+                wone++;
+            }
+            else
+            {
+                printf("Miss.\n");
+                oceanTwo[f1][f2] = MISSED;
+            }
+
+        }
+    }
+
+    if(wone == 3)
+    {
+        printf("%s Won the Game!!\n", playerOne);
+    }
+    else if(wtwo == 3)
+    {
+        printf("%s Won the Game!!\n", playerTwo);
+    }
+
 
     printOcean(oceanOne,playerOne);
     printf("\n\n");
     printOcean(oceanTwo,playerTwo);
 
-	return 0;
+    return 0;
 }
 
 void printIntro(void)
 {
 
 }
-void placeShip(int ocean[ROWS][COLS],char player[])
-{
-	int p1a, p1b, p1c;
-	int r;
-	int c;
-	int s;
-	
-	
-	printf("%s place your ship on a grid (0-9) \n Row(1) or Column(2): ", playerOne);
-	scanf("%d", &s);
-	
-	if(s == 1)
-	{
-	printf("Which row (0-9): ");
-	scanf("%d", &r);
-		while(r > 9 || r < 0){
-			printf("Invalid. Which row (0-9): ");
-			scanf("%d", &r);
-		}
-	printf("Point one: ");
-	scanf("%d", &p1a);
-		while(p1a > 9 || p1a < 0){
-			printf("Invalid. Point one: ");
-			scanf("%d" , &p1a);
-		}
-	printf("Point two: ");
-	scanf("%d", &p1b);
-		while(p1b > 9 || p1b < 0 || p1b == p1a || p1b < (p1a -1) || p1b > (p1a+1))
-		{
-			printf("Invalid. Point two: ");
-			scanf("%d", &p1b);
-		}
-	printf("Point three: ");
-	scanf("%d", &p1c);
-		while(p1c > 9 || p1c < 0 || p1c == p1a || p1c == p1b  || p1c < (p1b -1) || p1c > (p1b +1))
-		{
-			printf("Invalid. Point three: ");
-			scanf("%d", &p1c);
-		}
-		ocean[r][p1a]=OCCUPIED;
-		ocean[r][p1b]=OCCUPIED;
-		ocean[r][p1c]=OCCUPIED;
-	}
-	
-	
-	else if(s == 2) 
-	{
-	printf("Which Column(0-9): ");
-	scanf("%d", &c);
-		while(c > 9 || c < 0)
-		{
-			printf("Invalid. Which column (0-9): ");
-			scanf("%d", &c);
-		}
-	printf("Point one: ");
-	scanf("%d", &p1a);
-		while(p1a > 9 || p1a < 0){
-			printf("Invalid. Point one: ");
-			scanf("%d" , &p1a);
-		}
-	printf("Point two: ");
-	scanf("%d", &p1b);
-		while(p1b > 9 || p1b < 0 || p1b == p1a || p1b < (p1a -1) || p1b > (p1a+1))
-		{
-			printf("Invalid. Point two: ");
-			scanf("%d", &p1b);
-		}
-	printf("Point three: ");
-	scanf("%d", &p1c);
-		while(p1c > 9 || p1c < 0 || p1c == p1a || p1c == p1b  || p1c < (p1b -1) || p1c > (p1b +1))
-		{
-			printf("Invalid. Point three: ");
-			scanf("%d", &p1c);
-		}
-		ocean[p1a][c]=OCCUPIED;
-		ocean[p1b][c]=OCCUPIED;
-		ocean[p1c][c]=OCCUPIED;
-	}
-}
-
-
 void resetOcean(int ocean[ROWS][COLS])
 {
     int r,c;
@@ -279,6 +199,86 @@ void resetOcean(int ocean[ROWS][COLS])
         {
             ocean[r][c] = EMPTY;
         }
+    }
+}
+void placeShip(int ocean[ROWS][COLS],char player[])
+{
+    //variables
+    int p1a, p1b, p1c;
+    int r;
+    int c;
+    int s;
+
+    printf("%s place your ship on a grid (0-9) \n Row(1) or Column(2): ", player);
+    scanf("%d", &s);
+
+    if(s == 1)
+    {
+        printf("Which row (0-9): ");
+        scanf("%d", &r);
+        while(r > 9 || r < 0)
+        {
+            printf("Invalid. Which row (0-9): ");
+            scanf("%d", &r);
+        }
+        printf("Point one: ");
+        scanf("%d", &p1a);
+        while(p1a > 9 || p1a < 0)
+        {
+            printf("Invalid. Point one: ");
+            scanf("%d" , &p1a);
+        }
+        printf("Point two: ");
+        scanf("%d", &p1b);
+        while(p1b > 9 || p1b < 0 || p1b == p1a || p1b < (p1a -1) || p1b > (p1a+1))
+        {
+            printf("Invalid. Point two: ");
+            scanf("%d", &p1b);
+        }
+        printf("Point three: ");
+        scanf("%d", &p1c);
+        while(p1c > 9 || p1c < 0 || p1c == p1a || p1c == p1b  || p1c < (p1b -1) || p1c > (p1b +1))
+        {
+            printf("Invalid. Point three: ");
+            scanf("%d", &p1c);
+        }
+        ocean[r][p1a]=OCCUPIED;
+        ocean[r][p1b]=OCCUPIED;
+        ocean[r][p1c]=OCCUPIED;
+    }
+    else if(s == 2)
+    {
+        printf("Which Column(0-9): ");
+        scanf("%d", &c);
+        while(c > 9 || c < 0)
+        {
+            printf("Invalid. Which column (0-9): ");
+            scanf("%d", &c);
+        }
+        printf("Point one: ");
+        scanf("%d", &p1a);
+        while(p1a > 9 || p1a < 0)
+        {
+            printf("Invalid. Point one: ");
+            scanf("%d" , &p1a);
+        }
+        printf("Point two: ");
+        scanf("%d", &p1b);
+        while(p1b > 9 || p1b < 0 || p1b == p1a || p1b < (p1a -1) || p1b > (p1a+1))
+        {
+            printf("Invalid. Point two: ");
+            scanf("%d", &p1b);
+        }
+        printf("Point three: ");
+        scanf("%d", &p1c);
+        while(p1c > 9 || p1c < 0 || p1c == p1a || p1c == p1b  || p1c < (p1b -1) || p1c > (p1b +1))
+        {
+            printf("Invalid. Point three: ");
+            scanf("%d", &p1c);
+        }
+        ocean[p1a][c]=OCCUPIED;
+        ocean[p1b][c]=OCCUPIED;
+        ocean[p1c][c]=OCCUPIED;
     }
 }
 void printOcean(int ocean[ROWS][COLS],char player[])
