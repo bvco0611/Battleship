48395e73f6cb50057019ea9db0d1cd9b8ae7fed3
BRUNO VINICIUS CASSOL ORENGO <16104011@l1801141.portoalegre.pucrsnet.br>
2016-06-13 10:25:32 -0300

    ...

--- TrabF/biblioteca.c
+++ TrabF/biblioteca.c
@@ -28,6 +28,12 @@ void placeShip(int ocean[ROWS][COLS],char player[]){
     int r;
     int c;
     int s;
+    int p2a;
+    int p2b;
+
+
+    
+
 
     printf("%s place your ship on a grid (0-9) \n Row(1) or Column(2): ", player);
     scanf("%d", &s);
@@ -99,7 +105,75 @@ void placeShip(int ocean[ROWS][COLS],char player[]){
         ocean[p1a][c]=OCCUPIED;
         ocean[p1b][c]=OCCUPIED;
         ocean[p1c][c]=OCCUPIED;
+
     }
+        
+    printf("%s place your second ship on a grid (0-9) \n Row(1) or Column(2): ", player);
+    scanf("%d", &s);
+
+    if(s == 1)
+    {
+        printf("Which row (0-9): ");
+        scanf("%d", &r);
+        while(r > 9 || r < 0)
+        {
+            printf("Invalid. Which row (0-9): ");
+            scanf("%d", &r);
+        }
+        printf("Point one: ");
+        scanf("%d", &p2a);
+        while(p2a > 9 || p2a < 0 || p2a == p1a || p2a == p1b || p2a == p1c)
+        {
+            printf("Invalid. Point one: ");
+            scanf("%d" , &p2a);
+        }
+        printf("Point two: ");
+        scanf("%d", &p2b);
+        while(p2b > 9 || p2b < 0 || p2b == p2a || p2b < (p2a -1) || p2b > (p2a+1) || p2b == p1a || p2b ==p1b || p2b == p1c)
+        {
+            printf("Invalid. Point two: ");
+            scanf("%d", &p2b);
+        }
+        
+           
+        ocean[r][p2a]=OCCUPIED;
+        ocean[r][p2b]=OCCUPIED;
+        
+    }
+    else if(s == 2)
+    {
+        printf("Which Column(0-9): ");
+        scanf("%d", &c);
+        while(c > 9 || c < 0)
+        {
+            printf("Invalid. Which column (0-9): ");
+            scanf("%d", &c);
+        }
+        printf("Point one: ");
+        scanf("%d", &p1a);
+        while(p2a > 9 || p2a < 0 || p2a == p1a || p2a == p1b || p2a == p1c)
+        {
+            printf("Invalid. Point one: ");
+            scanf("%d" , &p2a);
+        }
+        printf("Point two: ");
+        scanf("%d", &p2b);
+        while(p2b > 9 || p2b < 0 || p2b == p2a || p2b < (p2a -1) || p2b > (p2a+1) || p2b == p1a || p2a == p1b || p2a == p1c)
+        {
+            printf("Invalid. Point two: ");
+            scanf("%d", &p2b);
+        }
+        
+        ocean[p2a][c]=OCCUPIED;
+        ocean[p2b][c]=OCCUPIED;
+        
+
+    }
+
+
+
+
+
 }
 
 void printOcean(int ocean[ROWS][COLS],char player[]){
